72a90668413acd6d9deb43f8955f4f2219182ae9
li chen <chenli_mail@163.com>
2019-09-28 18:41:16 +0800

    添加项目文件。

---
+++ XDataMidService/Controllers/XFileController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace XDATA2EAS.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class XFileController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Models.xfile> Get()
+        {
+            string itemclass = "select * from AuthorizeXFiles";
+            var tab_ic = SqlMapperUtil.SqlWithParams<Models.xfile>(itemclass, null);
+            return tab_ic;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+
+
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ XDataMidService/Models/xfile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XDATA2EAS.Models
+{
+    public class xfile
+    {
+        //TDS26MPvb select  name  , 'public string '+name+' { get; set; }' from  sys.columns where object_id in (select object_id from sys.objects where name ='AuthorizeXFiles')
+
+        public string CustomID { get; set; }
+        public string CustomName { get; set; }
+        public string FileName { get; set; }
+        public string ZTID { get; set; }
+        public string ZTName { get; set; }
+        public string ZTYear { get; set; }
+        public string BeginMonth { get; set; }
+        public string EndMonth { get; set; }
+        public string PZBeginDate { get; set; }
+        public string PZEndDate { get; set; }
+        public string MountType { get; set; }
+        public string MountTime { get; set; }
+        public string Currency { get; set; }
+        public int FileSize { get; set; }
+        public string UploadUser { get; set; }
+        public DateTime UploadTime { get; set; }
+    }
+}
